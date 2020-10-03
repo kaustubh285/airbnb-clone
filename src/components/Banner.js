c77e70051860e255d6d2c4f3a425eab7cbd6bf34
kaustubh <kaustubh285@gmail.com>
2020-10-04 01:10:03 +0530

    Date picker complete

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,21 @@
 import { Button } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import "./banner.css";
+import SearchDate from "./SearchDate";
 
 function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <div className='banner'>
       <div className='banner__search'>
-        <Button variant='outlined' className='banner__searchButton'>
-          Search Dates
+        <Button
+          variant='outlined'
+          className='banner__searchButton'
+          onClick={() => setShowSearch(!showSearch)}>
+          {!showSearch ? "Search Dates" : "Hide"}
         </Button>
+        {showSearch && <SearchDate />}
       </div>
       <div className='banner__info'>
         <h1>Go Near </h1>
@@ -16,7 +23,9 @@ function Banner() {
           Settle in somewhere new. Discover nearby stays to live, work, or just
           relax.
         </h5>
-        <Button variant='contained'>Explore nearby</Button>
+        <Button variant='contained' style={{ zIndex: 1 }}>
+          Explore nearby
+        </Button>
       </div>
     </div>
   );
